8f3880c3f46e73f98e7144d393c802c26f3340ef
Abdelrahman <abdelrahmanlotfy606@gmail.com>
2019-01-09 03:31:59 +0200

    angular

---
+++ chatAbdelrahman/src/app/components/signup/signup.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+  signupForm: FormGroup;
+  errorMessage: string;
+  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) { }
+
+  ngOnInit() {
+    this.init();
+  }
+  init() {
+    this.signupForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  signupUser() {
+    console.log(this.signupForm.value);
+    this.authService.registerUser(this.signupForm.value).subscribe((data)=> {
+      console.log(data);
+      this.signupForm.reset();
+      this.errorMessage = '';
+      this.router.navigate(['streams']);
+    }, 
+    (err)=> {
+      console.log(err);
+      if(err.error.msg) {
+        this.errorMessage = err.error.msg[0].message;
+      }
+      if(err.error.message) {
+        this.errorMessage = err.error.message;
+      }
+    });
+  }
+
+}
